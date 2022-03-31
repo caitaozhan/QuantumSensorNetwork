79781cc7d2c549228ae426e7fac9fe1fc27ac3c5
caitaozhan <caitaozhan@gmail.com>
2022-03-30 22:43:58 -0400

    hill climbing is beating guess in three sensors!

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ import time
 from plot import Plot
 
 
-def main():
+'''Two sensors
+'''
+def main2():
     print('Two sensors\n')
     seed = 2
     num_sensor = 2
@@ -88,5 +90,56 @@ def main():
     print(f'min error = {np.min(errors)}\nmax error = {np.max(errors)}\navg error = {np.average(errors)}')
     print(f'SDP time elpase = {elapse:.3}s')
 
+
+'''Three sensors
+'''
+def main3():
+    print('Three sensors\n')
+    seed = 2
+    num_sensor = 3
+    povm = Povm()
+    unitary_operator = random_unitary(dims=2, seed=seed)
+    Utility.print_matrix('Unitary matrix', unitary_operator._data)
+    optis = OptimizeInitialState(num_sensor)
+
+    # 1: optimize the initial state: Guess
+    optis.guess(unitary_operator)
+    print(optis)
+    priors = [0.4, 0.35, 0.25]
+    print('priors:', priors)
+    quantum_states = []
+    for i in range(num_sensor):
+        evolve_operator = Utility.evolve_operator(unitary_operator, num_sensor, i)
+        optis_copy = copy.deepcopy(optis)
+        optis_copy.evolve(evolve_operator)
+        quantum_states.append(optis_copy)
+    # Optimizing the POVM
+    povm.semidefinite_programming(quantum_states, priors, debug=False)
+    guess_success = povm.therotical_success
+    print(f'SDP error = {povm.theoretical_error}')
+
+
+    # 2: optimize the initial state: Hill climbing
+    mod_step = [0.1]*(2**num_sensor)
+    amp_step = [0.1]*(2**num_sensor)
+    decrease_rate = 0.96
+    iteration = 100
+    scores = optis.hill_climbing(unitary_operator=unitary_operator, priors=priors, seed=0, epsilon=Utility.EPSILON_HILLCLIMBING, \
+                                 mod_step=mod_step, amp_step=amp_step, decrease_rate=decrease_rate, min_iteration=iteration)
+    print(optis)
+    quantum_states = []
+    for i in range(num_sensor):
+        evolve_operator = Utility.evolve_operator(unitary_operator, num_sensor, i)
+        optis_copy = copy.deepcopy(optis)
+        optis_copy.evolve(evolve_operator)
+        quantum_states.append(optis_copy)
+    # Optimizing the POVM
+    povm.semidefinite_programming(quantum_states, priors, debug=False)
+    print(f'SDP error = {povm.theoretical_error}')
+    Plot.hillclimbing(scores, guess_success)
+
+
+
 if __name__ == '__main__':
-    main()
+    # main2()
+    main3()
