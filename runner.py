71697cddec9bd6b02c3581cb5161b8ed00b9ebe2
caitaozhan <caitaozhan@gmail.com>
2022-06-09 18:21:29 -0400

    minor

--- runner.py
+++ runner.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     # base_args = ["-us", "2", "-m", "Genetic algorithm", "Guess", "-mi", "100", "-ps", "32"]
     # base_args = ["-us", "2", "-m", "Particle swarm", "Guess", "-mi", "100", "-ps", "32"]
 
-    base_args = ["-us", "5", "-m", "Guess", "Hill climbing", "Particle swarm",  "-mi", "50", "-rn", "True", "-ps", "128"]
+    base_args = ["-us", "5", "-m", "Guess", "Hill climbing", "-mi", "50", "-rn", "True"]
 
     # 5 sensors experiment, in case it crashes again
     num_sensor  = 5
