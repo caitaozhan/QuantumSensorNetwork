4f85eaf9a3e37635ba1ce63d9e543d624038d6e9
caitaozhan <caitaozhan@gmail.com>
2022-12-24 22:59:33 -0500

    finish hill climbing 2,3,4 sensors experiment; finish comparing hill climbing, simulated annealing, and genetic algorithm

--- runner.py
+++ runner.py
@@ -65,14 +65,14 @@ if __name__ == '__main__':
     # base_args = ["-us", "2", "-m", "Genetic algorithm", "Guess", "-mi", "100", "-ps", "32"]
     # base_args = ["-us", "2", "-m", "Particle swarm", "Guess", "-mi", "100", "-ps", "32"]
 
-    base_args = ["-us", "2", "-m", "Hill climbing", "-mi", "100"]
+    base_args = ["-us", "2", "-m", "Hill climbing", "Simulated annealing", "Genetic algorithm", "-mi", "50", "-ps", "64"]
 
-    num_sensor  = 2
+    num_sensor  = 4
     equal       = True
     eval_metric = 'min error'  # 'min error' or 'unambiguous' or 'computational'
-    output_dir  = 'result2/12.22.2022'
-    output_file = 'varying_theta_2sensors'
-    thetas      = [i for i in range(1, 180)]
+    output_dir  = 'result2/12.23.2022'
+    output_file = 'compare_methods_4sensors'
+    thetas      = [i for i in range(40, 41)]
     start_seed  = [0]
 
     ps = []
