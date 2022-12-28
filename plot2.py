b4490a5a1b64f1c6d36b09df7d1b2f276b0a1fbd
caitaozhan <caitaozhan@gmail.com>
2022-12-27 19:12:32 -0500

    plotting

--- plot2.py
+++ plot2.py
@@ -8,8 +8,16 @@ from collections import defaultdict
 
 class Plot:
 
-    plt.rcParams['font.size'] = 65
-    plt.rcParams['lines.linewidth'] = 5
+    plt.rcParams['font.size'] = 60
+    plt.rcParams['lines.linewidth'] = 6
+
+    _METHOD = ['Hill climbing', 'Simulated annealing', 'Genetic algorithm']
+    _LABEL   = ['Hill Climbing', 'Simulated Annealing', 'Genetic Algorithm']
+    METHOD  = dict(zip(_METHOD, _LABEL))
+
+    _METHOD = ['Hill climbing', 'Simulated annealing', 'Genetic algorithm']
+    _STYLE  = ['solid',         'dashed',              'dotted']
+    LINE_STYLE = dict(zip(_METHOD, _STYLE))
 
     @staticmethod
     def vary_theta(data, filename):
@@ -34,8 +42,8 @@ class Plot:
                     y2.append(y[180-theta])  # 5 sensors doesn't has data for theta > 91 degrees
             Y[num_sensor] = y2
         
-        fig, ax = plt.subplots(1, 1, figsize=(30, 18))
-        fig.subplots_adjust(left=0.1, right=0.97, top=0.9, bottom=0.13)
+        fig, ax = plt.subplots(1, 1, figsize=(30, 15))
+        fig.subplots_adjust(left=0.1, right=0.97, top=0.9, bottom=0.15)
         ax.plot(X, Y[2], label='2 Sensors')
         ax.plot(X, Y[3], label='3 Sensors')
         ax.plot(X, Y[4], label='4 Sensors')
@@ -91,14 +99,48 @@ class Plot:
         
         # plotting
         methods = ['Hill climbing', 'Simulated annealing', 'Genetic algorithm']
-        fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(30, 18))
-        fig.subplots_adjust(left=0.1, right=0.97, top=0.9, bottom=0.13)
-        ax0.plot(X, Y[methods[0]])
-        ax0.plot(X, Y[methods[1]])
-        ax0.plot(X, Y[methods[2]])
-        ax1.plot(Y2[methods[0]])
-        ax1.plot(Y2[methods[1]])
-        ax1.plot(Y2[methods[2]])
+        fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(30, 17))
+        fig.subplots_adjust(left=0.1, right=0.97, top=0.85, bottom=0.2)
+        # ax0
+        ax0.plot(X, Y[methods[0]], label=Plot.METHOD[methods[0]], linestyle=Plot.LINE_STYLE[methods[0]])
+        ax0.plot(X, Y[methods[1]], label=Plot.METHOD[methods[1]], linestyle=Plot.LINE_STYLE[methods[1]])
+        ax0.plot(X, Y[methods[2]], label=Plot.METHOD[methods[2]], linestyle=Plot.LINE_STYLE[methods[2]])
+        ax0.legend(ncol=3, loc='upper left', bbox_to_anchor=(-0.2, 1.2, 0.05, 0.05), fontsize=55, columnspacing=0.8)
+        xticks = [i for i in range(0, 91, 15)]
+        ax0.set_xticks(xticks)
+        ax0.set_xticklabels([f'{x}' for x in xticks])
+        yticks = [0.2, 0.4, 0.6, 0.8, 1]
+        ax0.set_yticks(yticks)
+        ax0.set_yticklabels([f'{int(y * 100)}' for y in yticks])
+        ax0.set_ylim([0.25, 1.02])
+        ax0.vlines(x=40, ymin=0.2, ymax=0.883, linestyles='dotted', colors='black')
+        ax0.hlines(y=0.883, xmin=0,   xmax=40, linestyles='dotted', colors='black')
+        ax0.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
+        ax0.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
+        ax0.set_title('HC, SA, GA Performs Similary', fontsize=55)
+        ax0.set_xlabel('Theta (degrees)', labelpad=15)
+        ax0.set_xlim([0, 90])
+        ax0.set_ylim([0.2, 1.02])
+        ax0.set_ylabel('Success Probability (%)')
+        ax0.text(42, 0.865, '(40, 0.883)', fontsize=50)
+        # ax1
+        ax1.plot(Y2[methods[0]], linestyle=Plot.LINE_STYLE[methods[0]])
+        ax1.plot(Y2[methods[1]], linestyle=Plot.LINE_STYLE[methods[1]])
+        ax1.plot(Y2[methods[2]], linestyle=Plot.LINE_STYLE[methods[2]])
+        xticks = [i for i in range(0, 51, 10)]
+        ax1.set_xticks(xticks)
+        ax1.set_xticklabels([f'{x}' for x in xticks])
+        yticks = [0.8, 0.82, 0.84, 0.86, 0.88]
+        ax1.set_yticks(yticks)
+        ax1.set_yticklabels([f'{int(y * 100)}' for y in yticks])
+        ax1.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
+        ax1.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
+        ax1.set_title('Searching Process (Theta = 40)', fontsize=55)
+        ax1.set_xlabel('Iteration Number', labelpad=15)
+        ax1.set_xlim([-1, 50])
+        ax1.set_ylim([0.805, 0.89])
+        plt.figtext(0.28, 0.01, '(a)')
+        plt.figtext(0.75, 0.01, '(b)')
         fig.savefig(filename)
 
 
@@ -120,7 +162,7 @@ def methods_similar():
 
 
 if __name__ == '__main__':
-    # vary_theta()
+    vary_theta()
     methods_similar()
 
     
