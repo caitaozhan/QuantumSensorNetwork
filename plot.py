840ec958ddb3ac7f797e678b08cb235cc541e593
caitaozhan <caitaozhan@gmail.com>
2023-01-08 23:23:21 -0500

    minor

--- plot.py
+++ plot.py
@@ -1026,6 +1026,37 @@ def nonentangled_3sensors(guess_3s, hillclimb_3s):
     fig.savefig(filename)
 
 
+def conjecture_alltheta():
+    logs = ['result-tmp/1.8.2023/conjecture.4sensor']
+    data = Logger.read_log(logs)
+    filename = 'result-tmp/1.8.2023/conjecture.4sensor.png'
+
+    table = defaultdict(list)
+    for myinput, output_by_methods in data:
+        for method, output in output_by_methods.items():
+            table[method].append({myinput.unitary_theta: output.success})
+    Y = []
+    X = [i for i in range(1, 91)]
+    for method, mylist in table.items():
+        y = defaultdict(list)
+        for key_val in mylist:
+            for theta, success in key_val.items():
+                y[theta] = success
+        for theta in X:
+            if theta in y:
+                Y.append(y[theta])        
+    
+    fig, ax = plt.subplots(1, 1, figsize=(20, 16))
+    fig.subplots_adjust(left=0.15, right=0.97, top=0.88, bottom=0.15)
+    ax.plot(X, Y, label='Conjecture 1')
+    ax.legend()
+    ax.set_xlabel('Theta', labelpad=30)
+    ax.set_ylabel('Probability of Success', labelpad=30)
+    ax.set_title('Conjecture 1 for All Theta')
+    fig.savefig(filename)
+
+
+
 if __name__ == '__main__':
     # scores = [0.789991, 0.832989, 0.845341, 0.852194, 0.857062, 0.859864, 0.860891, 0.860928, 0.861471, 0.861524, 0.861533, 0.861536, 0.861537, 0.861541, 0.861543, 0.861763, 0.861993, 0.862333, 0.862427, 0.86244, 0.862667, 0.862849, 0.862851, 0.862853, 0.862854, 0.862854, 0.862855, 0.862856, 0.862856, 0.862857, 0.862857, 0.862857, 0.862858, 0.862858, 0.862858, 0.862858, 0.862858, 0.862858, 0.86286, 0.862944, 0.862949, 0.862949, 0.862949, 0.862949, 0.862949, 0.862949, 0.862949, 0.862955, 0.862989, 0.863003, 0.863005]
     # constant = 0.794494
@@ -1038,7 +1069,7 @@ if __name__ == '__main__':
     draw = True
     # guess_2s, hillclimb_2s = special_u_2sensor(draw)
     # guess_3s, hillclimb_3s = special_u_3sensor(draw)
-    guess_4s, hillclimb_4s = special_u_4sensor(draw)
+    # guess_4s, hillclimb_4s = special_u_4sensor(draw)
     # guess_5s, hillclimb_5s = special_u_5sensor(draw)
     # special_u_allsensors(guess_2s, hillclimb_2s, guess_3s, hillclimb_3s, guess_4s, hillclimb_4s, guess_5s, hillclimb_5s)
     # special_u_2()
@@ -1061,3 +1092,4 @@ if __name__ == '__main__':
     # varystartseed()
 
     
+    conjecture_alltheta()
--- runner.py
+++ runner.py
@@ -68,11 +68,11 @@ if __name__ == '__main__':
     # base_args = ["-us", "2", "-m", "Hill climbing", "Simulated annealing", "Genetic algorithm", "-mi", "50", "-ps", "64"]
     base_args = ["-us", "2", "-m", "Theorem"]
 
-    num_sensor  = 9
+    num_sensor  = 4
     equal       = True
     eval_metric = 'min error'  # 'min error' or 'unambiguous' or 'computational'
-    output_dir  = 'result2/12.31.2022'
-    output_file = 'conjecture.9sensor'
+    output_dir  = 'result-tmp/1.8.2023'
+    output_file = 'conjecture.4sensor'
     thetas      = [i for i in range(1, 91)]
     start_seed  = [0]
 
