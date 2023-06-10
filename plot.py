6e001e65b3ec4e6ea7fe3eff76d5e1dbd31b3f01
caitaozhan <caitaozhan@gmail.com>
2023-06-09 22:35:55 -0400

    update plots

--- plot.py
+++ plot.py
@@ -141,11 +141,16 @@ class Plot:
         ax.plot(X5[:65], Y5[methods[1]][:65], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=9)
         ax.plot(X5[65:], Y5[methods[1]][65:], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=9)
         arrowprops = dict(facecolor='black', width=5, headwidth=20)
-        ax.annotate('2 Sensor', xy=(25, 0.105), xytext=(1, 0.05), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('3 Sensor', xy=(30, 0.211), xytext=(45, 0.4), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('4 Sensor', xy=(28.3, 0.29), xytext=(45, 0.5), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('5 Sensor', xy=(26.6, 0.382), xytext=(45, 0.6), arrowprops=arrowprops, fontsize=50, va='center')
-        
+        ax.annotate('2 Sensor', xy=(154.5, 0.1), xytext=(158, 0.05), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('3 Sensor', xy=(162.5, 0.4),   xytext=(128, 0.4), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('4 Sensor', xy=(164, 0.5),  xytext=(128, 0.5), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('5 Sensor', xy=(166, 0.6), xytext=(128, 0.6), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.vlines(x=45,   ymin=-0.002, ymax=0.5, linestyles='solid', colors='grey', zorder=10)
+        ax.vlines(x=60,   ymin=-0.002, ymax=0.5, linestyles='solid', colors='grey', zorder=10)
+        ax.vlines(x=65.9, ymin=-0.002, ymax=0.43, linestyles='solid', colors='grey', zorder=10)
+        ax.text(34, 0.51, '$T$: 45    60')
+        ax.text(62, 0.44, '65.9')
+
         xticks = [i for i in range(0, 181, 15)]
         ax.set_xticks(xticks)
         ax.set_xticklabels([f'{x}' for x in xticks])
@@ -154,91 +159,12 @@ class Plot:
         ax.set_yticklabels([f'{int(y * 100)}' for y in yticks])
         ax.set_xlim([0, 180])
         ax.set_ylim([-0.002, 1])
-        # ax.vlines(x=60, ymin=0.2, ymax=1, linestyles='dotted', colors='black', )
         ax.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
         ax.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
-        ax.set_title('Empirical Validation of Heuristic Algorithms', fontsize=65, pad=40)
+        ax.set_title('Empirical Validation of Search Heuristics', fontsize=65, pad=40)
         ax.legend(ncol=2, loc='upper right',  fontsize=55, handlelength=3.5, edgecolor='black')
         ax.set_xlabel('Theta (degree)', labelpad=15)
         ax.set_ylabel('Probability of Error (%)', fontsize=60)
-        fig.savefig(filename)
-
-
-    @staticmethod
-    def methods_similar_old(data1: list, data2: list, filename: str):
-        # data1: varying theta, 4 sensors
-        table1 = defaultdict(list)
-        for myinput, output_by_methods in data1:
-            for method, output in output_by_methods.items():
-                table1[method].append({myinput.unitary_theta: output.success})
-        Y = defaultdict(list)
-        X = [i for i in range(1, 91)]
-        for method, mylist in table1.items():
-            y = defaultdict(list)
-            for key_val in mylist: # each theta only one experiment
-                for theta, success in key_val.items():
-                    y[theta] = success
-            y2 = []
-            for theta in X:
-                if theta in y:
-                    y2.append(y[theta])
-                else:
-                    raise Exception(f'data missing: theta={theta}')
-            Y[method] = y2
-
-        # data2: varying iteration, 4 sensors, theta = 40 case
-        table2 = {}
-        for myinput, output_by_methods in data2:
-            for method, output in output_by_methods.items():
-                table2[method] = output.scores[:50]
-        Y2 = table2
-        
-        # plotting
-        arrowprops = dict(facecolor='black', width=5, headwidth=25)
-        methods = ['Hill climbing', 'Simulated annealing', 'Genetic algorithm']
-        fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(30, 17))
-        fig.subplots_adjust(left=0.1, right=0.97, top=0.83, bottom=0.19)
-        # ax0
-        ax0.plot(X, Y[methods[0]], label=Plot.METHOD[methods[0]], linestyle=Plot.LINE_STYLE[methods[0]], color=Plot.COLOR[methods[0]], linewidth=9)
-        ax0.plot(X, Y[methods[1]], label=Plot.METHOD[methods[1]], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=7)
-        ax0.plot(X, Y[methods[2]], label=Plot.METHOD[methods[2]], linestyle=Plot.LINE_STYLE[methods[2]], color=Plot.COLOR[methods[2]], linewidth=5)
-        ax0.legend(ncol=3, loc='upper left', bbox_to_anchor=(-0.2, 1.23, 0.05, 0.05), fontsize=55, columnspacing=0.8, edgecolor='black')
-        xticks = [i for i in range(0, 91, 15)]
-        ax0.set_xticks(xticks)
-        ax0.set_xticklabels([f'{x}' for x in xticks])
-        yticks = [0.2, 0.4, 0.6, 0.8, 1]
-        ax0.set_yticks(yticks)
-        ax0.set_yticklabels([f'{int(y * 100)}' for y in yticks])
-        ax0.set_ylim([0.25, 1.02])
-        ax0.vlines(x=40, ymin=0.2, ymax=0.883, linestyles='dotted', colors='black')
-        ax0.hlines(y=0.883, xmin=0,   xmax=40, linestyles='dotted', colors='black')
-        ax0.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
-        ax0.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
-        ax0.set_title('HC, SA, GA Performs Similary', fontsize=55, pad=20)
-        ax0.set_xlabel('Theta (degree)', labelpad=15)
-        ax0.set_xlim([0, 90])
-        ax0.set_ylim([0.2, 1.02])
-        ax0.set_ylabel('Probability of Success (%)')
-        ax0.annotate('(40, 0.883)', xy=(42, 0.883), xytext=(51, 0.883), arrowprops=arrowprops, fontsize=50, va='center')
-        # ax1
-        ax1.plot(Y2[methods[0]], linestyle=Plot.LINE_STYLE[methods[0]], color=Plot.COLOR[methods[0]], linewidth=9)
-        ax1.plot(Y2[methods[1]], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=7)
-        ax1.plot(Y2[methods[2]], linestyle=Plot.LINE_STYLE[methods[2]], color=Plot.COLOR[methods[2]], linewidth=5)
-        xticks = [i for i in range(0, 51, 10)]
-        ax1.set_xticks(xticks)
-        ax1.set_xticklabels([f'{x}' for x in xticks])
-        yticks = [0.8, 0.82, 0.84, 0.86, 0.88]
-        ax1.set_yticks(yticks)
-        ax1.set_yticklabels([f'{int(y * 100)}' for y in yticks])
-        ax1.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
-        ax1.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
-        ax1.set_title('Searching Process (Theta = 40)', fontsize=55, pad=20)
-        ax1.set_xlabel('Iteration Number', labelpad=15)
-        ax1.set_xlim([-1, 50])
-        ax1.set_ylim([0.805, 0.89])
-        ax1.annotate('Random Initial State', xy=(0.7, 0.811), xytext=(6, 0.811), arrowprops=arrowprops, fontsize=50, va='center')
-        plt.figtext(0.28, 0.01, '(a)')
-        plt.figtext(0.75, 0.01, '(b)')
         fig.savefig(filename)
 
 
@@ -281,72 +207,9 @@ class Plot:
 
 
     @staticmethod
-    def lemma2_old(data, filename):
-        plt.rcParams['font.size'] = 45
-        fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(2, 2, figsize=(22, 15))
-        fig.subplots_adjust(left=0.14, right=0.97, top=0.88, bottom=0.12, wspace=0.4, hspace=0.3)
-        
-        boxprops = dict(linewidth=5)
-        medianprops = dict(linewidth=5)
-        whiskerprops = dict(linewidth=5)
-        capprops = dict(linewidth=5)
-
-        n = 2
-        print(data[f'n{n}.perm'][0], min(data[f'n{n}.avg']), max(data[f'n{n}.avg']))
-        ax0.hlines(y=data[f'n{n}.perm'][0], xmin=0.85, xmax=1.15, linewidth=5)
-        ax0.boxplot(data[f'n{n}.avg'], whis=(0, 100), widths=0.2, boxprops=boxprops, medianprops=medianprops, whiskerprops=whiskerprops, capprops=capprops)
-        xticks = [1]
-        ax0.set_xticks(xticks)
-        ax0.set_xticklabels([f'{x+1}' for x in xticks])
-        yticks = [0.069, 0.070, 0.071, 0.072, 0.073]
-        ax0.set_yticks(yticks)
-        ax0.set_yticklabels([f'{round(y*100, 1)}' for y in yticks])
-        ax0.tick_params(axis='x', direction='in', length=8, width=2, pad=15)
-        ax0.tick_params(axis='y', direction='in', length=8, width=2, pad=15)
-        n = 3
-        print(data[f'n{n}.perm'][0], min(data[f'n{n}.avg']), max(data[f'n{n}.avg']))
-        ax1.hlines(y=data[f'n{n}.perm'][0], xmin=0.85, xmax=1.15, linewidth=5)
-        ax1.boxplot(data[f'n{n}.avg'], whis=(0, 100), widths=0.2, boxprops=boxprops, medianprops=medianprops, whiskerprops=whiskerprops, capprops=capprops)
-        ax1.set_xticks(xticks)
-        ax1.set_xticklabels([f'{x+2}' for x in xticks])
-        yticks = [0.189, 0.191, 0.193, 0.195, 0.197]
-        ax1.set_yticks(yticks)
-        ax1.set_yticklabels([f'{round(y*100, 1)}' for y in yticks])
-        ax1.tick_params(axis='x', direction='in', length=8, width=2, pad=15)
-        ax1.tick_params(axis='y', direction='in', length=8, width=2, pad=15)
-        n = 4
-        print(data[f'n{n}.perm'][0], min(data[f'n{n}.avg']), max(data[f'n{n}.avg']))
-        ax2.hlines(y=data[f'n{n}.perm'][0], xmin=0.85, xmax=1.15, linewidth=5)
-        ax2.boxplot(data[f'n{n}.avg'], whis=(0, 100), widths=0.2, boxprops=boxprops, medianprops=medianprops, whiskerprops=whiskerprops, capprops=capprops)
-        ax2.set_xticks(xticks)
-        ax2.set_xticklabels([f'{x+3}' for x in xticks])
-        yticks = [0.182, 0.186, 0.190, 0.194, 0.198]
-        ax2.set_yticks(yticks)
-        ax2.set_yticklabels([f'{round(y*100, 1)}' for y in yticks])
-        ax2.tick_params(axis='x', direction='in', length=8, width=2, pad=15)
-        ax2.tick_params(axis='y', direction='in', length=8, width=2, pad=15)
-        n = 5
-        print(data[f'n{n}.perm'][0], min(data[f'n{n}.avg']), max(data[f'n{n}.avg']))
-        ax3.hlines(y=data[f'n{n}.perm'][0], xmin=0.85, xmax=1.15, linewidth=5)
-        ax3.boxplot(data[f'n{n}.avg'], whis=(0, 100), widths=0.2, boxprops=boxprops, medianprops=medianprops, whiskerprops=whiskerprops, capprops=capprops)
-        ax3.set_xticks(xticks)
-        ax3.set_xticklabels([f'{x+4}' for x in xticks])
-        yticks = [0.224, 0.227, 0.23, 0.233, 0.236]
-        ax3.set_yticks(yticks)
-        ax3.set_yticklabels([f'{round(y*100, 1)}' for y in yticks])
-        ax3.tick_params(axis='x', direction='in', length=8, width=2, pad=15)
-        ax3.tick_params(axis='y', direction='in', length=8, width=2, pad=15)
-        # fig
-        fig.supylabel('Probability of Error (%)')
-        fig.supxlabel('Number of Sensors')
-        fig.suptitle('$Lemma\ 2$: The Averaged Initial States Have Lower $PoE$')
-        fig.savefig(filename)
-
-
-    @staticmethod
     def lemma2(data, filename):
-        fig, axes = plt.subplots(1, 4, figsize=(40, 18))
-        fig.subplots_adjust(left=0.1, right=0.97, top=0.82, bottom=0.1, wspace=0.4, hspace=0.3)
+        fig, axes = plt.subplots(1, 4, figsize=(40, 21))
+        fig.subplots_adjust(left=0.1, right=0.97, top=0.87, bottom=0.09, wspace=0.4, hspace=0.3)
         
         for i, t in enumerate([26, 46, 66, 86]):
             y_min, y_max, y = [], [], []
@@ -359,24 +222,24 @@ class Plot:
             y = np.array(y) * 100
             X = [3,4,5]
             yerr = np.stack([y - y_min, y_max - y])
-            axes[i].errorbar(X, y, yerr=yerr, linewidth=5, capsize=20, capthick=5, fmt=' ', marker='.', markersize=30, color='r', ecolor='b')
+            axes[i].errorbar(X, y, yerr=yerr, linewidth=5, capsize=20, capthick=5, fmt=' ', marker='.', markersize=45, color='r', ecolor='b')
             axes[i].yaxis.grid()
             axes[i].set_xlim([2.7, 5.3])
             axes[i].set_title(f'Theta={t}', fontsize=60, pad=20)
-            axes[i].set_xlabel('Number of Sensors', fontsize=55, labelpad=10)
+            axes[i].set_xlabel('Number of Sensors', fontsize=55, labelpad=15)
             axes[i].tick_params(axis='y', direction='in', length=10, width=4)
             axes[i].tick_params(pad=10)
         
         fig.supylabel('Probability of Error (%)')
         # fig.supxlabel('Theta (degree)')
-        fig.suptitle('$Lemma\ 2$: The Averaged Initial States Have Lower $PoE$')
+        fig.suptitle('$Conjecture\ 2$: The Averaged Initial States Have Lower $PoE$', fontsize=75)
         fig.savefig(filename)
 
 
     @staticmethod
     def lemma3(data, filename):
-        fig, ax = plt.subplots(1, 1, figsize=(24, 14))
-        fig.subplots_adjust(left=0.12, right=0.98, top=0.89, bottom=0.16)
+        fig, ax = plt.subplots(1, 1, figsize=(26, 16))
+        fig.subplots_adjust(left=0.12, right=0.98, top=0.91, bottom=0.14)
 
         X = np.linspace(0, 0.99, 100)
         X = np.append(X, [0.995, 0.998, 0.999, 0.99999])
@@ -400,9 +263,9 @@ class Plot:
         ax.set_ylim([-0.001, 0.801])
         ax.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
         ax.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
-        ax.set_xlabel('$x$ (such that $\\forall i \\neq j$: $\langle \phi_i \| \phi_j \\rangle = x$)', labelpad=22)
-        ax.set_ylabel('Probability of Error (%)', labelpad=30, fontsize=58)
-        ax.set_title('$Lemma\ 3$: $PoE$ Increase with the Increase in $x$', pad=40, fontsize=58)
+        ax.set_xlabel('$x$ (such that $\\forall i \\neq j$: $\langle \phi_i \| \phi_j \\rangle = x$)', labelpad=20)
+        ax.set_ylabel('Probability of Error (%)', labelpad=40, fontsize=58)
+        ax.set_title('$Conjecture\ 3$: $PoE$ Increase with the Increase in $x$', pad=50, fontsize=58)
         ax.grid()
         fig.savefig(filename)
 
@@ -516,11 +379,16 @@ class Plot:
         ax.plot(X5[:65], Y5[methods[1]][:65], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=9)
         ax.plot(X5[65:], Y5[methods[1]][65:], linestyle=Plot.LINE_STYLE[methods[1]], color=Plot.COLOR[methods[1]], linewidth=9)
         arrowprops = dict(facecolor='black', width=5, headwidth=20)
-        ax.annotate('2 Sensor', xy=(25, 0.105), xytext=(1, 0.05), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('3 Sensor', xy=(30, 0.211), xytext=(45, 0.4), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('4 Sensor', xy=(28.3, 0.29), xytext=(45, 0.5), arrowprops=arrowprops, fontsize=50, va='center')
-        ax.annotate('5 Sensor', xy=(26.6, 0.382), xytext=(45, 0.6), arrowprops=arrowprops, fontsize=50, va='center')
-        
+        ax.annotate('2 Sensor', xy=(154.5, 0.1), xytext=(158, 0.05), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('3 Sensor', xy=(162.5, 0.4),   xytext=(128, 0.4), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('4 Sensor', xy=(164, 0.5),  xytext=(128, 0.5), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.annotate('5 Sensor', xy=(166, 0.6), xytext=(128, 0.6), arrowprops=arrowprops, fontsize=50, va='center')
+        ax.vlines(x=45,   ymin=-0.002, ymax=0.5, linestyles='solid', colors='grey', zorder=10)
+        ax.vlines(x=60,   ymin=-0.002, ymax=0.5, linestyles='solid', colors='grey', zorder=10)
+        ax.vlines(x=65.9, ymin=-0.002, ymax=0.43, linestyles='solid', colors='grey', zorder=10)
+        ax.text(34, 0.51, '$T$: 45    60')
+        ax.text(62, 0.44, '65.9')
+
         xticks = [i for i in range(0, 181, 15)]
         ax.set_xticks(xticks)
         ax.set_xticklabels([f'{x}' for x in xticks])
@@ -532,7 +400,7 @@ class Plot:
         # ax.vlines(x=60, ymin=0.2, ymax=1, linestyles='dotted', colors='black', )
         ax.tick_params(axis='x', direction='in', length=10, width=3, pad=15)
         ax.tick_params(axis='y', direction='in', length=10, width=3, pad=15)
-        ax.set_title('Empirical Validation of Conjecture', fontsize=65, pad=40)
+        ax.set_title('Empirical Validation of $Conjecture$ 1', fontsize=65, pad=40)
         ax.legend(ncol=2, loc='upper right',  fontsize=55, handlelength=3.5, edgecolor='black')
         ax.set_xlabel('Theta (degree)', labelpad=15)
         ax.set_ylabel('Probability of Error (%)', fontsize=60)
@@ -740,8 +608,8 @@ def symmetry():
 
 if __name__ == '__main__':
     # vary_theta()
-    methods_similar()
+    # methods_similar()
     # lemma2()
     # lemma3()
-    # conjecture()
+    conjecture()
     # symmetry()
